444bfe46c779022ed9bceeea11ef0136960b5f73
Devon <loudywen@gmail.com>
2016-03-30 17:43:26 -0500

    switch from table to grid. fixed grid style

--- VaadinAwesome/src/main/java/com/vaadin/devon/dummyImpl/Grid1.java
+++ VaadinAwesome/src/main/java/com/vaadin/devon/dummyImpl/Grid1.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.util.StringUtils;
 
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.Widgetset;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.devon.dummy.GridDesign;
 import com.vaadin.devon.entity.TemperatureRecords;
 import com.vaadin.devon.entity.User;
 import com.vaadin.devon.serviceinterface.TemperatureRecordsDAO;
@@ -19,27 +19,14 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
-import com.vaadin.ui.Grid.SelectionMode;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
 
-@SpringView(name = "Dummy Grid")
-
-@Widgetset("MyAppWidgetset")
+@SpringView(name ="Grid")
 @UIScope
-public class DummyGrid extends VerticalLayout implements View {
-
-	private Grid grid = new Grid();
-	//private Table t = new Table();
-	private TextField filter = new TextField();
-	private Button addNewBtn = new Button();
-
+@Theme("dashboard")
+public class Grid1 extends GridDesign implements View {
+	
 	@Autowired
 	@Qualifier("TemperatureRecordsImpl")
 	private TemperatureRecordsDAO tRDAO;
@@ -47,32 +34,19 @@ public class DummyGrid extends VerticalLayout implements View {
 	@Autowired
 	private TemperatureRecords tr;
 
-	@Override
-	public void enter(ViewChangeEvent event) {
-
-	}
-
-
 	@PostConstruct
-	public void init() {
-		
-		//setMargin(true);
+	protected void init(){
+		setMargin(true);
 		setSpacing(true);
-		//setSizeFull();
-		addComponent(toolBar());
-	//	addComponent(buildTable());
+		
 		listTemp(null);
 		grid.setSizeFull();
-		
 		grid.setColumnOrder("id","email","temperature","device","timeStamp");
 		addComponent(grid);
 
+	
 	}
-
-	private void addCompents(TextField filter2, Button addNewBtn2) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 	private void listTemp(String text) {
 		if (StringUtils.isEmpty(text)) {
@@ -86,14 +60,13 @@ public class DummyGrid extends VerticalLayout implements View {
 			 */
 		}
 	}
-
-	private Component toolBar() {
-		HorizontalLayout actions = new HorizontalLayout();
-		actions.addComponent(filter);
-		actions.addComponent(addNewBtn);
-		actions.setSpacing(true);
-		return actions;
+	
+	
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
-
+	
 
 }
